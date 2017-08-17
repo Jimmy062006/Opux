9d5abf744c4488194db6951f56f18ee1e2e58ef4
Jimmy06 <robert@myhomedns.co.uk>
2017-08-18 00:42:50 +0100

    Fixed Role Removal Stuffz

--- src/Opux/Functions.cs
+++ src/Opux/Functions.cs
@@ -999,7 +999,7 @@ namespace Opux
                                     try
                                     {
                                         //Check if roles when should not have any
-                                        if (corps.Count > 0 && !corps.ContainsValue(corporationid.ToString()) && alliance.Count > 0 && !alliance.ContainsValue(allianceID))
+                                        if (!corps.ContainsValue(corpID) && !alliance.ContainsValue(allianceID))
                                         {
                                             if (discordUser != null)
                                             {
