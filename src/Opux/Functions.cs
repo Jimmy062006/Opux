de91aec3664d49d4b32b0da83fe6dd52b34f32b1
Jimmy06 <robert@myhomedns.co.uk>
2018-01-10 05:34:41 +0000

    Percentage

--- src/Opux/Functions.cs
+++ src/Opux/Functions.cs
@@ -3305,7 +3305,7 @@ namespace Opux
                         .AddInlineField("Last Seen:", $"{lastSeenTime}")
                         .AddField("Extra", $"\u200b")
                         .AddInlineField("Threat", $"Dangerous {characterStats.dangerRatio}% {text1} {100 - characterStats.dangerRatio}% Snuggly")
-                        .AddInlineField("In Fleet", $"{characterStats.gangRatio}% {text2} {characterStats.gangRatio}%")
+                        .AddInlineField("In Fleet", $"IN {characterStats.gangRatio}% {text2} OUT {100 - characterStats.gangRatio}%")
                         .AddInlineField("Chance of Fleet", $"{chanceText}");
 
                     var embed = builder.Build();
