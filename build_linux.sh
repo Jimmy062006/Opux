e76e633d8ec68f7c775a5d2c857a9321fd12e837
Jimmy06 <robert@myhomedns.co.uk>
2018-02-17 13:15:27 +0000

    Updated Linux build script

--- build_linux.sh
+++ build_linux.sh
@@ -4,6 +4,6 @@ echo Building Opux2
 dotnet build
 cd Opux2
 echo Publishing Opux 2 to $mypath/Release
-rmdir /S $mypath/Release
-dotnet publish -c Release --output $%mypath\Release
-xcopy $mypath/Opux2/bin/Debug/netcoreapp2.0/Plugins $mypath/Release/Plugins /e /s /i /y /Exclude:$mypath/exclusion_list
+rm -Rf $mypath/Release
+dotnet publish -c Release --output $mypath/Release
+rsync -avz --exclude='BuildEvents.dll' --exclude='BuildEvents.pdb' $mypath/Opux2/bin/Debug/netcoreapp2.0/Plugins/ $mypath/Release/Plugins/
